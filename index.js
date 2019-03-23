f4acdcdbcda1d45e390fd27994f710f619c6f505
Kian Edalat <akedalat@gmail.com>
2019-03-23 22:43:04 +0000

    done

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ superbowlWin = (record) => {
   let result = record.find( record => record.result === "W" )
   return !!result ? result.year : undefined
 }
+
+function superbowlWin(array){
+return array.find(record => record.result === "W")
+}
