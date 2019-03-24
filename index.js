51d4a42b15362a478f0b0fea04707fa14cbf1562
SofiaFredriksson <sofiapkf@gmail.com>
2019-03-24 22:55:36 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr) {
+  let result = arr.find(record => record.result === "W")
+if (result){
+  return result.year
+} else {
+  return undefined
+}
+
+}
